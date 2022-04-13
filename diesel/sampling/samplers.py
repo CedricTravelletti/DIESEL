ba0094f7a77de9b3daa23ebfdb633f20be0dd5fa
Cedric Travelletti <cedrictravelletti@gmail.com>
2022-04-13 14:25:36 +0200

    SVD sampler works and tested. BEAUTIFUL.

---
+++ diesel/sampling/samplers.py
@@ -0,0 +1,48 @@
+""" Module for sampling from multivariate gaussians.
+
+"""
+import dask.array as da
+
+
+class SvdSampler:
+    """ Sample multivariate gaussian from SVD decomposition of 
+    its covariance matrix.
+
+    Parameters
+    ----------
+    svd_u: dask_array
+        Left singular vectors of the covariance matrix 
+        (as obtained from da.linalg.svd_compressed).
+    svd_s: dask_array
+        Vector of singular values of the covariance matrix.
+
+    """
+    def __init__(self, svd_u, svd_s):
+        self.svd_u, self.svd_s = svd_u, svd_s
+
+        # Equivalent of Cholesky matrix in traditional sampling.
+        smat = da.diag(self.svd_s)
+        self.cholesky_lazy = da.dot(self.svd_u, smat)
+
+        self.dim = svd_u.shape[0]
+
+    def sample(self, n_samples):
+        """ Generate samples.
+
+        Parameters
+        ----------
+        n_samples: int
+            Number of independent samples to generate.
+
+        Returns
+        -------
+        samples: array [n_samples, dim]
+
+        """
+        samples = da.random.normal(size=(n_samples, self.svd_u.shape[1], 1))
+        samples = da.dot(self.cholesky_lazy, samples).squeeze().T
+        return samples
+
+
+
+
---
+++ tests/test_SvdSampler.py
@@ -0,0 +1,42 @@
+""" Tests for diesel.sampling.SvdSampler
+
+"""
+import dask.array as da
+from dask.distributed import Client
+from diesel.gridding import SquareGrid
+from diesel.cluster import LocalCluster
+from diesel.covariance import matern32
+from diesel.sampling import SvdSampler
+
+
+cluster = LocalCluster()
+client = Client(cluster)
+
+# Build a square grid with 30^2 elements.
+grid = SquareGrid(30)
+grid_pts = grid.grid_pts
+
+# Construct (lazy) covariance matrix.
+lazy_covariance_matrix = matern32(grid_pts, lambda0=0.2)
+
+# Compute compressed SVD.
+svd_rank = 900
+u, s, v = da.linalg.svd_compressed(
+                lazy_covariance_matrix, k=svd_rank, compute=False) 
+
+# Construct sampler from the svd of the covariance matrix.
+sampler = SvdSampler(u, s)
+
+# Sample 16 ensemble members.
+ensembles = sampler.sample(16)
+
+ensembles = client.compute(ensembles).result()
+
+# Plot results
+import matplotlib.pyplot as plt
+fig, axs = plt.subplots(4, 4)
+
+for i, sample in enumerate(ensembles):
+    axs.flatten()[i].imshow(grid.list_to_mesh(sample))
+
+plt.show()
